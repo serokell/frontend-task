210f101ffd274014ea8458b5cf075d2123265b11
ilyas ahmed <ilyasahmed081@gmail.com>
2021-04-03 01:03:16 +0500

    home services and contact sections done

--- serokell-landingpage/src/app/components/homepage/homepage-routing.module.ts
+++ serokell-landingpage/src/app/components/homepage/homepage-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomepageComponent } from './homepage.component';
-import { ServicesComponent } from './services/services.component';
-import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
